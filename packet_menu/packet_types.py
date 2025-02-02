35d12525763fd7142635c718951c2ba1896a86bd
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-02 13:28:43 -0500

    First commit of the day. Working on finishing my packet classes.

--- packet_menu/packet_types.py
+++ packet_menu/packet_types.py
@@ -5,23 +5,24 @@
 
 import numpy as np
 from dataclasses import dataclass
-
+from datetime import datetime
 
 @dataclass
-class Packet:
+class EthernetPacket:
     destination_mac: bytearray # Offset: Bytes 1-6. Destination MAC is the first 6 bytes
     source_mac: bytearray # Offset: Bytes 7-12. Source MAC is the second set of 6 bytes
     ethernet_type: bytearray # Offset: Bytes 13-14. This is 2 bytes and it indicates the type of data in the payload of the ethernet frame
 
 
+@dataclass
+class TCP:
+
 
 @dataclass
 class DHCP:
 
-
-
 @dataclass
-class TCP:
+class DNS:
 
 
 @dataclass
