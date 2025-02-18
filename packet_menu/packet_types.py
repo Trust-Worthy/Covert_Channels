02ee6c005763f6a70219bd6622f66923c8d21eef
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-18 09:24:27 -0500

    Creating the HTTP class.

--- packet_menu/packet_types.py
+++ packet_menu/packet_types.py
@@ -194,6 +194,7 @@ class ARP(Ethernet_Packet):
     target_hardware_address: bytes  # Bytes 32-37: Target MAC address (6 bytes)
     target_protocol_address: bytes  # Bytes 38-41: Target IP address (4 bytes)
 
+
     @classmethod
     def from_bytes(cls, data: bytes) -> "ARP":
         if len(data) < 28:
@@ -215,23 +216,89 @@ class ARP(Ethernet_Packet):
             packet_data_byte=data,
             packet_data_np=np.frombuffer(data, dtype=np.uint8),
         )
+    
 
+@dataclass
+class ARP_REQUEST(ARP):
+    pass
 
+@dataclass
+class ARP_REPLY(ARP):
+    pass
+
+@dataclass
+class ICMP_REQUEST:
+    pass
+
+@dataclass
+class ICMP_REPLY:
+    pass
+
+@dataclass
+class HTTP_Packet(TCP_Packet):
+    is_request: Optional[bool] = None  # True if request, False if response
+    method: Optional[bytes] = None  # Present in requests
+    request_uri: Optional[bytes] = None  # Present in requests
+    http_version: Optional[bytes] = None  # Present in both request/response
+    
+    status_code: Optional[bytes] = None  # Present in responses
+    status_message: Optional[bytes] = None  # Present in responses
+    
+    headers: Optional[dict] = None  # Common in both
+    body: Optional[bytes] = None  # Optional in both
+    
+    def __init__(self, raw_data: bytes):
+        """Parses an HTTP request or response from raw bytes."""
+        try:
+            # Split request into headers and body
+            header_section, body_section = raw_data.split(b"\r\n\r\n", 1) if b"\r\n\r\n" in raw_data else (raw_data, b"")
+
+            # Split headers into lines
+            header_lines = header_section.split(b"\r\n")
+            
+            # First line determines if it's a request or response
+            first_line_parts = header_lines[0].split(b" ")
+            
+            if first_line_parts[0].startswith(b"HTTP/"):  
+                # This is an **HTTP Response**
+                self.is_request = False
+                self.http_version = first_line_parts[0]  # First part is HTTP version (e.g., HTTP/1.1)
+                self.status_code = first_line_parts[1]  # Second part is status code (e.g., 200)
+                self.status_message = b" ".join(first_line_parts[2:])  # Remaining is status message
+            else:
+                # This is an **HTTP Request**
+                self.is_request = True
+                self.method = first_line_parts[0]  # First word (e.g., GET, POST)
+                self.request_uri = first_line_parts[1]  # Second word (e.g., /index.html)
+                self.http_version = first_line_parts[2]  # Third word (e.g., HTTP/1.1)
+            
+            # Parse headers
+            self.headers = {}
+            for line in header_lines[1:]:  # Skip first line
+                if b": " in line:
+                    key, value = line.split(b": ", 1)
+                    self.headers[key.decode()] = value.decode()
+            
+            # Store body if present
+            self.body = body_section if body_section else None
+
+        except Exception as e:
+            print(f"Error parsing HTTP packet: {e}")
 
 
 
 @dataclass
 class DNS:
+    pass
 
-
-
+@dataclass
+class QUIC:
+    pass
 
 
 @dataclass
-class ICMP
-
-@dataclass
-class OTHER
+class OTHER:
+    pass
 
 
 '''
