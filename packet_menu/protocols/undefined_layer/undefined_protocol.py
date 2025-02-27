1c018c40786825ad263809774c31661bacad7e0e
Trust-Worthy <jondeveloper0@gmail.com>
2025-02-27 18:12:59 -0500

    almost Finished the undefined protocol class.

--- packet_menu/protocols/undefined_layer/undefined_protocol.py
+++ packet_menu/protocols/undefined_layer/undefined_protocol.py
@@ -1,4 +1,21 @@
 
-@dataclass
+from protocols.layer_2_protocols.ethernet import Ethernet_Frame
+from cleaning_captures.packet_parser import Packet_parser
+
+
+
+
 class OTHER_PROTOCOL:
-    pass
+    
+    def __init__(self, all_bytes: bytearray, parser: Packet_parser):
+        
+        self._parser: Packet_parser = parser
+        self._parser.packet_type = type(self)
+        self._other_protocol_size: int = len(all_bytes)
+
+    @property
+    def other_protocol_size(self) -> int:
+        return self.other_protocol_size
+    @other_protocol_size.setter
+    def other_protocol_size(self, value: int):
+        self._other_protocol_size = value
