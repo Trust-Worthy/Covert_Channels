a0a3da030f40585187c143fa205357c288e02961
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-27 12:25:51 -0600

    Added ethernet type property to Ethernet class.

--- src/covert_hunter/protocols/ethernet.py
+++ src/covert_hunter/protocols/ethernet.py
@@ -11,17 +11,34 @@ class EthernetFrame:
     """
     def __init__(self, timestamp: datetime, all_bytes:bytes, parser: Packet_Parser = None):
         
-        self._timestamp: datetime = timestamp
+        self._timestamp: datetime = None
         self._destination_mac: bytes = None
         self._source_mac: bytes = None
+        self._ethernet_type: bytes = None
+        
+        self.parse_ethernet_frame(timestamp, all_bytes)
         
         
         
-    def parse_ethernet_frame(self, all_bytes: bytes) -> None:
+    def parse_ethernet_frame(self, timestamp:datetime, all_bytes: bytes) -> None:
         
+        self.timestamp = timestamp
         self.destination_mac = all_bytes[0:6]
-        self.source_mac
+        self.source_mac = all_bytes[6:12]
+        # ethernet type next
         
+        
+    @property
+    def timestamp(self) -> datetime:
+        return self._timestamp
+    
+    @timestamp.setter
+    def timestamp(self, date: datetime):
+        
+        if type(date) != datetime:
+            raise TypeError("Date is not a datetime object.")
+        
+        self._timestamp = date
         
     @property
     def destination_mac(self) -> bytes:
@@ -31,7 +48,7 @@ class EthernetFrame:
     def destination_mac(self, data: bytes):
         
         if len(data) != 6:
-            raise ValueError("Destination MAC address must be 6 bytes long")
+            raise ValueError("Destination MAC address must be 6 bytes long.")
         
         self._destination_mac = data
     
@@ -45,5 +62,5 @@ class EthernetFrame:
         if len(data) != 6:
             raise ValueError("Source MAC address must be 6 bytes long")
         
-        self.source_mac = data
+        self._source_mac = data
     
