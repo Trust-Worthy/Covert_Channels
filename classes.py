a62f60c93d17d8b1018528aba025a24e8461016f
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-27 11:49:35 -0600

    Writing out all of the classes that I want for my model.

---
+++ classes.py
@@ -0,0 +1,25 @@
+"""_summary_
+
+12 classes
+
+
+Layer 2
+Ethernet Frame
+
+Layer 3
+Arp
+ICMP
+IP
+
+Layer 4
+TCP
+UDP
+
+Application Layer
+DNS ** Most important
+HTTP
+HTTPS
+QUIC
+TLS
+Undefined 
+"""
