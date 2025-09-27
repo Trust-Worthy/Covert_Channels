2967f5009efe5326c8e6eef09970c086b9b8b1a5
Trust-Worthy <jondeveloper0@gmail.com>
2025-09-27 09:40:32 -0600

    Fresh start.

--- __main__.py
+++
@@ -1,5 +0,0 @@
-# __main__.py
-from cli.menu import main
-
-if __name__ == "__main__":
-    main()
--- core/processing/packet_pandas.py
+++
@@ -1,5 +0,0 @@
-'''
-I think it would be really useful to potentially convert the packet data to a pandas dataframe?
-
-
-'''
